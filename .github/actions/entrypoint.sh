7abdc30a310532215487a99f495af99c2061499d
jason <chrispnugget88@yahoo.com>
2023-01-25 13:19:35 -0500

    commit

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -22,7 +22,7 @@ fi
 
 echo "Cleaning source folder"
 
-echo "Removing the folling files... ${INPUT_CLEAN_FILES[@]}"
+# echo "Removing the folling files... ${INPUT_CLEAN_FILES[@]}"
 
 for files in "${INPUT_CLEAN_FILES[@]}"; do
   rm -rf "$INPUT_DESTINATION_FOLDER/$files"
